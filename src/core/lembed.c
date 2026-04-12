c0355c2129f11791bc52f8aede84ce4a991db60e
samyeyo <samir.tine@luart.org>
2026-04-12 20:47:41 +0200

    Faster embedded modules loading Fixed random crash on x86 when loading embedded modules Multiple compiled executables instances are now possible

--- src/core/lembed.c
+++ src/core/lembed.c
@@ -1,6 +1,6 @@
 /*
  | LuaRT - A Windows programming framework for Lua
- | Luart.org, Copyright (c) Tine Samir 2025
+ | Luart.org, Copyright (c) Tine Samir 2026
  | See Copyright Notice in LICENSE.TXT
  |-------------------------------------------------
  | lembed.c | LuaRT embed module implementation
@@ -10,6 +10,7 @@
 
 #include "lrtapi.h"
 #include <luart.h>
+#include "lua\llimits.h"
 
 #include <File.h>
 #include <shlwapi.h>
@@ -28,19 +29,62 @@ struct zip_t;
 
 typedef void (*voidf)(void);
 extern int Zip_extract(lua_State *L); 
-extern struct zip_t *fs;
+
+typedef struct {
+	struct zip_t *zip;
+	wchar_t       tmp[MAX_PATH];
+	HANDLE        hMap;
+	HANDLE        hFile;
+	uint8_t       *map;
+} ZipFs;
+extern ZipFs fs;
+
 BYTE *datafs  = NULL;
 wchar_t path[MAX_PATH];
+wchar_t uniquePath[512] =  {0};
 
-struct zip_t *open_fs(void *ptr, size_t size) {
-  char * buffer = (char*)ptr;
-  DWORD old, new;
+#define WIDE2(x) L##x
+#define WIDE(x)  WIDE2(x)
 
-  if (VirtualProtect(ptr, 1, PAGE_READWRITE, &old)) {
-    *buffer = 0x50;   
-    VirtualProtect(ptr, 1, old, &new);
+BOOL open_fs(void *ptr, size_t size) {
+  wchar_t tempPath[MAX_PATH];
+  ULONGLONG size64 = (ULONGLONG)size;
+
+  GetTempPathW(MAX_PATH, tempPath);
+  GetTempFileNameW(tempPath, L"ZIP", 0, fs.tmp);
+
+  fs.hFile = CreateFileW(fs.tmp, GENERIC_READ|GENERIC_WRITE, 0, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_TEMPORARY, NULL);
+  fs.hMap = CreateFileMappingW(fs.hFile, NULL, PAGE_READWRITE, (DWORD)(size64 >> 32), (DWORD)(size64 & 0xFFFFFFFF), NULL);
+  fs.map = MapViewOfFile(fs.hMap, FILE_MAP_WRITE, 0, 0, size);
+
+  if (!fs.map)
+          return FALSE;
+  uint8_t *enc = (uint8_t *)ptr;
+  uint8_t *map = (uint8_t *)fs.map;
+  uint8_t key = (uint8_t)size;
+
+  const size_t CHUNK = 1 << 20;
+  size_t offset = 0;
+
+  while (offset < size) {
+      size_t n = (size - offset > CHUNK) ? CHUNK : (size - offset);
+      for (size_t i = 0; i < n; i++)
+          map[offset + i] = enc[offset + i] ^ key;
+      offset += n;
   }
-  return zip_mem_new(ptr, size);
+  fs.zip = zip_mem_new(fs.map, size);
+  return fs.zip != NULL;
+}
+
+static BOOL CreateUniqueTempDir() {
+    if (GetTempFileNameW(temp_path, WIDE(LUA_VERSION_MAJOR "." LUA_VERSION_MINOR "." LUA_VERSION_RELEASE), 0, uniquePath) == 0)
+        return FALSE;
+    DeleteFileW(uniquePath);
+
+    if (!CreateDirectoryW(uniquePath, NULL))
+        return FALSE;
+
+    return (wcslen(uniquePath) + 1 < 512);
 }
 
 static const char *getnextfilename (char **path, char *end) {
@@ -89,9 +133,10 @@ static const char *searchpath (lua_State *L, const char *name,
   while ((filename = getnextfilename(&pathname, endpathname)) != NULL) {
     filename = luaL_gsub(L, filename, "\\", "/");
     if (filename[0] == '.' && filename[1] == '/')
-      filename += 2; 
+      filename += 2;
+    lua_pop(L, 1); 
     CharLowerA((char *)filename);
-    if ((zip_entry_open(fs, filename) == 0))
+    if ((zip_entry_open(fs.zip, filename) == 0))
       /* does file exist and is readable? */
       return lua_pushstring(L, filename);  /* save and return name */
   }
@@ -119,12 +164,12 @@ static int searcher_embedded_Lua (lua_State *L) {
 	  int idx = 0;
 	  size_t size;
 
-    zip_entry_read(fs, (void**)&buff, &size);
+    zip_entry_read(fs.zip, (void**)&buff, &size);
     if (memcmp(buff, "\xEF\xBB\xBF", 3)==0)
       idx = 3;
     if (luaL_loadbuffer(L, (const char*)(buff+idx), size-idx, filename))
       luaL_error(L, "error loading module '%s' from embedded file '%s':\n\t%s", lua_tostring(L, 1), filename, lua_tostring(L, -1));
-    zip_entry_close(fs);
+    zip_entry_close(fs.zip);
     lua_pushstring(L, filename); 
     return 2;   
   }
@@ -156,12 +201,13 @@ static int searcher_embedded_C (lua_State *L) {
   if (filename) { 
 found:
     lua_getfield(L, LUA_REGISTRYINDEX, CMEMLIBS);
-    lua_pushwstring(L, temp_path);
-    lua_pushstring(L, filename);
+    lua_pushwstring(L, uniquePath);
+    lua_pushfstring(L, "/%s", filename);
     lua_concat(L, 2);
-    if (zip_entry_open(fs, filename) == 0) { 
+    if (zip_entry_open(fs.zip, filename) == 0) { 
       wchar_t *tmp = lua_towstring(L, -1);
-      if ((make_path(tmp) && (zip_entry_fread(fs, lua_tostring(L, -1)) == 0))) {
+      make_path(tmp);
+      if (zip_entry_fread(fs.zip, lua_tostring(L, -1)) == 0) {
         HMODULE hm;
         char *modpath = get_directory(filename);
         
@@ -176,12 +222,12 @@ found:
                 char fname[512];
                 wchar_t dllpath[512];
                 snprintf(modname, 512, "%s/%s", modpath, importName);
-                snprintf(fname, 512, "%ls\\%s", temp_path, importName);
-                _snwprintf(dllpath, 512, L"%ls\\%hs", temp_path, modpath);
-                if (zip_entry_open(fs, modname) == 0) {
+                snprintf(fname, 512, "%ls\\%s", uniquePath, importName);
+                _snwprintf(dllpath, 512, L"%ls\\%hs", uniquePath, modpath);
+                if (zip_entry_open(fs.zip, modname) == 0) {
                     make_path(dllpath); 
-                    zip_entry_fread(fs, fname);
-                    zip_entry_close(fs);
+                    zip_entry_fread(fs.zip, fname);
+                    zip_entry_close(fs.zip);
                 }
                 importDescriptor++;
             }
@@ -204,7 +250,7 @@ found:
         free(modpath);
       }
       free(tmp);
-      zip_entry_close(fs);
+      zip_entry_close(fs.zip);
       if (lua_isfunction(L, -1)) {
         lua_pushstring(L, filename); 
         return 2;   
@@ -220,11 +266,10 @@ found:
   return 1;
 }
 
-
-
 //-------------------------------------------------[luaL_embedclose() luaRT C API]
 LUA_API int luaL_embedclose(lua_State *L) {
   if (lua_getfield(L, LUA_REGISTRYINDEX, CMEMLIBS)) {
+    wchar_t doubleNull[514];
     wchar_t *path;
     lua_pushnil(L);
     while (lua_next(L, -2)) {
@@ -233,6 +278,19 @@ LUA_API int luaL_embedclose(lua_State *L) {
       DeleteFileW(path);
       free(path);
       lua_pop(L, 1);
+    }
+
+    SHFILEOPSTRUCTW op = {0};
+    op.wFunc  = FO_DELETE;
+    op.pFrom  = doubleNull;
+    op.fFlags = FOF_NOCONFIRMATION | FOF_NOERRORUI | FOF_SILENT;
+    SHFileOperationW(&op);
+
+    if (fs.zip) {
+      UnmapViewOfFile(fs.map);
+      CloseHandle(fs.hMap);
+      CloseHandle(fs.hFile);
+      DeleteFileW(fs.tmp);
     }
   }
 	return 0;
@@ -245,7 +303,7 @@ LUA_API BYTE *luaL_embedopen(lua_State *L) {
     HGLOBAL hdata = LoadResource(NULL, hres);
     datafs = LockResource(hdata);
     size_t size = SizeofResource(NULL, hres);
-    if (datafs && (size > 2) && (fs = open_fs(datafs, size))) {
+    if (datafs && (size > 2) && open_fs(datafs, size)) {
       lua_Integer len;
       lua_getglobal(L, "package");
       lua_getfield(L, -1, "searchers");
@@ -258,10 +316,10 @@ LUA_API BYTE *luaL_embedopen(lua_State *L) {
       lua_rawseti(L, -2, ++len);
       lua_pop(L, 2);
       SetDefaultDllDirectories(LOAD_LIBRARY_SEARCH_DEFAULT_DIRS);
-      AddDllDirectory(temp_path);
-    } else fs = NULL;
+      AddDllDirectory(uniquePath);
+    };
   }
-  return (BYTE*)fs;
+  return (BYTE*)fs.zip;
 }
 
 LUA_METHOD(embed, File) {
@@ -269,7 +327,7 @@ LUA_METHOD(embed, File) {
     lua_pushcfunction(L, Zip_extract);
     lua_getfield(L, -2, "zip");
     lua_pushvalue(L, 1);
-    lua_pushwstring(L, temp_path);
+    lua_pushwstring(L, uniquePath);
     lua_pcall(L, 3, LUA_MULTRET, 0);
     return 1;
 }
@@ -305,6 +363,8 @@ static const luaL_Reg embed_properties[] = {
 
 //-------------------------------------------------[luaopen_embed() "embed" module]
 LUAMOD_API int luaopen_embed(lua_State *L) {
+  if (!CreateUniqueTempDir())
+    return luaL_error(L, "failed to access system temporary folder");
   luaL_getsubtable(L, LUA_REGISTRYINDEX, CMEMLIBS);
 	lua_registermodule(L, "embed", embedlib, embed_properties, luaL_embedclose);
   lua_getglobal(L, "package");
@@ -312,7 +372,7 @@ LUAMOD_API int luaopen_embed(lua_State *L) {
   lua_getfield(L, -1, "compression");
   lua_pushstring(L, "zip");
   lua_getfield(L, -2, "Zip");
-  lua_pushlightuserdata(L, fs);
+  lua_pushlightuserdata(L, fs.zip);
   lua_pcall(L, 1, 1, 0);
   lua_rawset(L, -6);
   lua_pop(L, 1);
