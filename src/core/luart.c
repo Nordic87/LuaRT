24054c7807a5decfaea6c63f42a1fb29c3c12e3c
samyeyo <samir.tine@luart.org>
2026-04-12 20:51:34 +0200

    New options -p -l and -v Interpreters updated for Lua 5.5 VM

--- src/core/luart.c
+++ src/core/luart.c
@@ -1,6 +1,6 @@
 /*
  | LuaRT - A Windows programming framework for Lua
- | Luart.org, Copyright (c) Tine Samir 2025
+ | Luart.org, Copyright (c) Tine Samir 2026
  | See Copyright Notice in LICENSE.TXT
  |-------------------------------------------------
  | luart.c | LuaRT interpreter
@@ -20,6 +20,7 @@
 #include <shlwapi.h>
 #include <shlobj_core.h>
 #include <luart.h>
+#include "lpreprocess.h"
 #include "resources\resource.h"
 
 #ifndef _MSC_VER
@@ -38,30 +39,45 @@ extern BYTE *datafs;
 static lua_State *L;
 static WCHAR exename[MAX_PATH];
 
+
 #ifdef RTC
 
 #include <stdio.h>
 #include <stdint.h>
 #include <File.h>
 
-int link(lua_State *L) {
+extern void xor_buffer_lenbytes(uint8_t *data, size_t len);
+
+static int link(lua_State *L) {
 	wchar_t *fname = lua_towstring(L, 1);
 	wchar_t *fexe = lua_towstring(L, 2);
-	HANDLE hFile = CreateFileW(fname, GENERIC_READ, 0, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
-	DWORD FileSize = GetFileSize(hFile, NULL);
-	BYTE *pBuffer = calloc(1, FileSize);
-	DWORD dwBytesRead;
-	HANDLE hExeFile;
-	BOOL result = FALSE;
-	
-	if (ReadFile(hFile, pBuffer, FileSize, &dwBytesRead, NULL)) {
-		hExeFile = BeginUpdateResourceW(fexe, FALSE);
-		*pBuffer = 0x4C;
-		result = UpdateResource(hExeFile, RT_RCDATA, MAKEINTRESOURCE(EMBED), MAKELANGID(LANG_ENGLISH, SUBLANG_ENGLISH_US), (LPVOID)pBuffer, FileSize);
+	HANDLE hFile = CreateFileW(fname, GENERIC_READ | GENERIC_WRITE, 0, NULL, OPEN_EXISTING, 0, NULL);
+	DWORD sizeHigh, sizeLow = GetFileSize(hFile, &sizeHigh);
+	uint64_t size = ((uint64_t)sizeHigh << 32) | sizeLow;
+	HANDLE hMap = CreateFileMappingW(hFile, NULL, PAGE_READWRITE, sizeHigh, sizeLow, NULL);
+	uint8_t *map = MapViewOfFile(hMap, FILE_MAP_READ | FILE_MAP_WRITE, 0, 0, 0);
+	int result = FALSE;
+
+	if (map) {
+		BOOL result = FALSE;
+		uint8_t key = (uint8_t)size;
+		const size_t CHUNK = 1 << 20;
+
+		for (uint64_t off = 0; off < size; off += CHUNK) {
+			size_t n = (size - off > CHUNK) ? CHUNK : (size - off);
+			uint8_t *p = map + off;
+
+			for (size_t i = 0; i < n; i++)
+				p[i] ^= key;
+		}
+		HANDLE hExe = BeginUpdateResourceW(fexe, FALSE);
+		result = UpdateResource(hExe, RT_RCDATA, MAKEINTRESOURCE(EMBED), MAKELANGID(LANG_ENGLISH, SUBLANG_ENGLISH_US), map, (DWORD)size);
+		EndUpdateResource(hExe, FALSE);
+		UnmapViewOfFile(map);
+		result = TRUE;
 	}
-	EndUpdateResource(hExeFile, FALSE);
+	CloseHandle(hMap);
 	CloseHandle(hFile);
-	free(pBuffer);
 	free(fname);
 	free(fexe);
 	lua_pushboolean(L, result);
@@ -171,18 +187,6 @@ static int update_exe_icon(lua_State *L) {
 }
 #endif
 
-void lua_stop(void) {
-	if (L) {
-		if (lua_getfield(L, LUA_REGISTRYINDEX, "atexit") == LUA_TFUNCTION) {
-			if (lua_pcall(L, 0, 0, 0))
-				puts(lua_tostring(L, -1));
-		}
-		lua_close(L);
-		CoUninitialize();
-		L = NULL;
-	}
-}
-
 #ifdef RTWIN
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR lpCmdLine, INT nCmdShow) {
 #else
@@ -192,7 +196,7 @@ int main() {
 	int i, result = EXIT_SUCCESS;
 	BOOL is_embeded = FALSE;
 	wchar_t **envp, **wargv;
-	int argc, si = 0, argfile = 1;
+	int argc, si = 0, script_index = 0;
 	Task *t;
 #ifdef _MSC_VER	
     static int (*__wgetmainargs)(int *, wchar_t ***, wchar_t ***, int, int *);
@@ -208,7 +212,8 @@ int main() {
 	InitCommonControlsEx(&icex);
 	CoInitializeEx(NULL, COINIT_APARTMENTTHREADED);
 	L = luaL_newstate();
-	luaL_openlibs(L);
+	lua_openmodules(L);
+	lua_warning(L, "@off", 0);
 	GetModuleFileNameW(NULL, (WCHAR*)exename, sizeof(exename));
 	if ((is_embeded = (luaL_embedopen(L) != NULL))) {
 		luaL_requiref(L, "embed", luaopen_embed, 2);
@@ -227,91 +232,148 @@ int main() {
 		lua_pop(L, 1);
 #endif
 	}
-
+#ifndef RTSTATIC	
 	atexit(lua_stop);
+#endif
 #ifdef RTC
 	lua_register(L, "seticon", update_exe_icon);
 	lua_register(L, "link", link);
 #endif
-	if (argc == 1 && !is_embeded)
-		puts(LUA_VERSION " " LUA_ARCH " - Windows programming framework for Lua.\nCopyright (c) 2025, Samir Tine.\nusage:\tluart.exe [-e statement] [script] [args]\n\n\t-e statement\tExecutes the given Lua statement\n\tscript\t\tRun a Lua script file\n\targs\t\tArguments for Lua interpreter");
-	else {
-		lua_createtable(L, argc, 0);	
-		for (i=1; i < argc+is_embeded; i++) {
+	// --- Argument Parsing and Handling ---
+
+	if (argc == 1 && !is_embeded) {
+help:
+		puts(LUA_VERSION " " LUA_ARCH " - Windows programming framework for Lua.\nCopyright (c) 2065, Samir Tine.\nusage:\tluart.exe [options] [script] [args]\n\n\t-e stat\t\tExecutes the given Lua statement\n\t-l name\t\tRequire library 'name'\n\t-v\t\tShow version information\n\tscript\t\tRun a Lua script file\n\targs\t\tArguments for Lua interpreter");
+	} else {
+		// 1. Identify script index
+		if (is_embeded) {
+			script_index = 0; 
+		} else {
+			for (i = 1; i < argc; i++) {
+				if (wargv[i][0] == L'-') {
+					switch(wargv[i][1]) {
+						case L'e':
+						case L'p':
+						case L'l':
+						case L'v':
+							break;
+						default:
+							lua_pushfstring(L, "error: unknown option '%s'", __argv[i]);
+							goto error;
+					}
+				} else {
+					script_index = i;
+					break;
+				}
+			}
+		}
+
+		// 2. Build 'arg' table
+		lua_createtable(L, argc, 0);
+		for (i = 1; i < argc + is_embeded; i++) {
 			lua_pushwstring(L, wargv[i-is_embeded]);
-			lua_rawseti(L, -2, i-1);
-		}	
+			lua_rawseti(L, -2, i - 1);
+		}
 		lua_pushwstring(L, exename);
 		lua_rawseti(L, -2, -1);
 		lua_setglobal(L, "arg");
 		lua_gc(L, LUA_GCGEN, 0, 0);
-		
-		if (is_embeded) {	
-			if (luaL_loadstring(L, "load(embed.File('__mainLuaRTStartup__.lua'):open():read())()"))
-				goto error;
-			goto compiledscript;
-		}
-		else {
-			if (*wargv[1] == L'-') {
-				if ((wargv[1][1] == L'e')) {
-					if (argc > 2) {
-						lua_pushwstring(L, wargv[2]);
-						if (luaL_loadstring(L, lua_tostring(L, -1)) == LUA_OK) {
-							is_embeded = TRUE;
-							goto compiledscript;
-						} 
-						fputs(lua_tostring(L, -1), stderr);
-						fputs("\n", stderr);
-					} else
-						fputs("error: -e option expects a statement argument\n", stderr);					
-				} else {
-					lua_pushstring(L, "error: unknown option ");
-					lua_pushwstring(L, wargv[1]);
-					lua_concat(L, 2);
-					goto error;
-				}
-			} else { 
-				if (luaL_loadfile(L, __argv[argfile]) == LUA_OK) {
-compiledscript:		t = (Task*)lua_pushinstance(L, Task, 1);
-					if (lua_pcall(L, 0, 0, 0)) 
-						goto error;
-					do {
-						if (!lua_schedule(L)) {
-error:			
-#ifdef RTWIN
-							size_t len;
-							const char *err = lua_tolstring(L, -1, &len);	
-							if (len)	{	
-								if (is_embeded) {
-									if (strstr(err, "[string """) == err) {
-										const char *tmp = luaL_gsub(L, err, "[string \"", "");
-										err = luaL_gsub(L, tmp, "\"]:", ":");
-										len = strlen(err);
-									}
-								}
-								lua_pushstring(L, err);							
-								wchar_t *msg = lua_towstring(L, -1);		
-								MessageBoxW(NULL, msg, L"Runtime error", MB_ICONERROR | MB_OK);
-								free(msg);					
-							}
-#else
-							const char *err = lua_tostring(L, -1);
-							if (is_embeded) {
-								if (strstr(err, "[string """) == err) {
-									const char *tmp = luaL_gsub(L, err, "[string \"", "");
-									err = luaL_gsub(L, tmp, "\"]:", ":");
-								} 
-							}
-							fputs(err, stderr);
-							fputs("\n", stderr);
-#endif
-							result = EXIT_FAILURE;
+
+		// 3. Process Options (Pass 2)
+		if (!is_embeded) {
+			for (i = 1; i < argc; i++) {
+				if (script_index > 0 && i >= script_index) break; 
+
+				if (wargv[i][0] == L'-') {
+					switch(wargv[i][1]) {
+						case L'p':
+							char* output = preprocess_file(L, __argv[++i]);
+							if (!output) goto error;
+							fputs(output, stdout);
+							free(output);
+							return EXIT_SUCCESS;
+						case L'v':
+							fputs(LUA_VERSION " " LUA_ARCH " Copyright (c) 2026, Samir Tine.\n", stdout);
+							if (argc == 2) return EXIT_SUCCESS; 
 							break;
-						}				
-					} while (t->status != TTerminated);
-				} else goto error;
+						case L'l': 
+							{
+								if (wcslen(wargv[i]) == 2) {
+									lua_pushstring(L, "error: expecting module name after -l option");
+									goto error;
+								}
+								lua_getglobal(L, "require");
+								lua_pushwstring(L, wargv[i]+2);
+								if (lua_pcall(L, 1, 1, 0) >= LUA_ERRRUN) goto error;
+								lua_setglobal(L, __argv[i]+2);
+							}
+							break;
+						case L'e':
+							{
+								lua_pushwstring(L, wargv[++i]);
+								if (luaL_loadstring(L, lua_tostring(L, -1)) != LUA_OK) goto error;
+								lua_remove(L, -2); 
+								if (lua_pcall(L, 0, 0, 0) != LUA_OK) goto error;
+								lua_stop();
+								return 0;
+							}
+							break;
+					}
+				}
 			}
 		}
+
+		// 4. Load and Run Script
+		if (is_embeded) {	
+			if ( luaL_loadbufferx(L, "require('__mainLuaRTStartup__')", sizeof("require('__mainLuaRTStartup__')")-1, __argv[0], "t") != LUA_OK )
+				goto error;
+		} else {
+			if (script_index > 0) {
+				if (luaL_loadfilep(L, __argv[script_index], NULL) != LUA_OK) 
+					goto error;
+			} else
+				goto help;
+		}
+
+		// Execute the loaded chunk (embedded or file)
+		t = (Task*)lua_pushinstance(L, Task, 1);
+		if (lua_pcall(L, 0, 0, 0)) 
+			goto error;
+
+		do {
+			if (!lua_schedule(L)) {
+error:			
+#ifdef RTWIN
+				size_t len;
+				const char *err = lua_tolstring(L, -1, &len);	
+				if (len)	{	
+					if (is_embeded) {
+						if (strstr(err, "[string """) == err) {
+							const char *tmp = luaL_gsub(L, err, "[string \"", "");
+							err = luaL_gsub(L, tmp, "\"]:", ":");
+							len = strlen(err);
+						}
+					}
+					lua_pushstring(L, err);							
+					wchar_t *msg = lua_towstring(L, -1);		
+					MessageBoxW(NULL, msg, L"Runtime error", MB_ICONERROR | MB_OK);
+					free(msg);					
+				}
+#else
+				const char *err = lua_tostring(L, -1);
+				if (is_embeded) {
+					if (strstr(err, "[string """) == err) {
+						const char *tmp = luaL_gsub(L, err, "[string \"", "");
+						err = luaL_gsub(L, tmp, "\"]:", ":");
+					} 
+				}
+				fputs(err, stderr);
+				fputs("\n", stderr);
+#endif
+				result = EXIT_FAILURE;
+				break;
+			}  
+		} while (t->status != TTerminated);
 	}
 	lua_stop();
 	return result;
